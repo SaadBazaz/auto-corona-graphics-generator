61b77c1c594e9de7e26adc8ce774832f00105680
Saad A. Bazaz <51885228+SaadBazaz@users.noreply.github.com>
2020-04-10 23:32:08 +0500

    Update upload_pic.py

--- src/upload_pic.py
+++ src/upload_pic.py
@@ -2,6 +2,6 @@ from instabot import Bot
 import os
 
 bot = Bot()
-bot.login(username="boringtestb", password="Wannabeonforbes98.")
+bot.login(username="YOUR_USERNAME", password="YOUR_PASSWORD")
 # followers = bot.get_user_followers(bot.user_id)
 bot.upload_photo(os.path.abspath('../AIs/Upload_test.jpg'),'Testing Map Upload via code...')
