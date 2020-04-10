ca48261a4e1ecb7c4f566029d1c0ec03052df3b8
Saad A. Bazaz <51885228+SaadBazaz@users.noreply.github.com>
2020-04-10 23:31:34 +0500

    Delete upload_pic.py

--- upload_pic.py
+++
@@ -1,6 +0,0 @@
-from instabot import Bot
-
-bot = Bot()
-bot.login(username="boringtestb", password="Wannabeonforbes98.")
-# followers = bot.get_user_followers(bot.user_id)
-bot.upload_photo('./Top_20_Recovered.jpg','hello graphic upload')
